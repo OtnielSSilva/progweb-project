9892c19b76792a25ed94e58f23005a499b53c002
OtnielSSilva <otniel.dss04@gmail.com>
2024-08-05 11:25:50 -0400

    Refatora componente Header para responsividade em telas menores

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,32 +18,20 @@ const Header: React.FC<HeaderProps> = ({ onSearch, onProfileClick }) => {
 	};
 
 	return (
-		<header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+		<header className="bg-gray-800 text-white p-4 flex justify-between items-center md:flex-row flex-col">
 			<div className="text-2xl font-semibold">
 				{/* Your logo or app name here */}
 				Cinephile's <span className="text-blue-500">Picks</span>
 			</div>
-
-			<nav className="space-x-4">
-				<a href="/" className="hover:text-gray-300">
-					Home
-				</a>
-				<a href="/movies" className="hover:text-gray-300">
-					Movies
-				</a>
-				<a href="/tv-shows" className="hover:text-gray-300">
-					TV Shows
-				</a>
-			</nav>
-
-			<div className="flex items-center">
+			{/* Search input and button */}
+			<div className="flex mt-4 md:mt-0 w-full md:w-auto items-center">
 				<input
 					type="text"
 					placeholder="Search..."
 					value={searchQuery}
 					onChange={handleSearchChange}
 					onKeyDown={(e) => e.key === "Enter" && handleSearchSubmit()}
-					className="bg-gray-700 rounded-l py-2 px-4 focus:outline-none focus:bg-gray-600"
+					className="bg-gray-700 rounded-l py-2 px-4 focus:outline-none focus:bg-gray-600 w-full"
 				/>
 				<button
 					onClick={handleSearchSubmit}
@@ -54,7 +42,19 @@ const Header: React.FC<HeaderProps> = ({ onSearch, onProfileClick }) => {
 				</button>
 			</div>
 
-			<div>
+			{/* Hidden on smaller screens */}
+			<nav className="md:space-x-4 md:flex hidden">
+				<a href="/" className="hover:text-gray-300">
+					Home
+				</a>
+				<a href="/movies" className="hover:text-gray-300">
+					Movies
+				</a>
+				<a href="/tv-shows" className="hover:text-gray-300">
+					TV Shows
+				</a>
+			</nav>
+			<div className="md:block hidden">
 				<button onClick={onProfileClick} className="hover:text-gray-300">
 					{/* Profile icon here */}
 					Profile
